1285c10250ebf5118cc63e2dc24a04aa6b0b0adf
ArmanGrewal007 <armansinghgrewal@gmail.com>
2024-10-02 20:33:48 +0530

    Renamed

--- app.py
+++ app.py
@@ -44,4 +44,4 @@ def get():
 
 if __name__ == "__main__":
     import uvicorn, os
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", 8000)), reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=int(os.getenv("PORT", 8000)), reload=True)
